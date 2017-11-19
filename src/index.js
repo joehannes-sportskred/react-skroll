2f353d420247f3e3e01c2e8e33ee0060f55da28b
Johannes Neugschwentner <johannes@sportskred.com>
2017-11-19 19:06:49 -0400

    export statements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-export Scroller from './Scroller'
-export ScrollLink from './ScrollLink'
-export ScrollProvider from './ScrollProvider'
-export scrollConnect from './scrollConnect'
-export * as utilities from './utilities'
-export default exports
+export { Scroller } from './Scroller';
+export { ScrollLink } from './ScrollLink';
+export { ScrollProvider } from './ScrollProvider';
+export { scrollConnect } from './scrollConnect';
+export * as utilities from './utilities';
